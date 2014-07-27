3b4e2481a7d9ce7aefa51a47f7575e8527cd1fc0
anurag <test@test.com>
2014-07-27 13:13:34 +0530

    Here we are showing "SOP" can able to print all type of  literals

---
+++ src/com/anurag/initial/Basic2.java
@@ -0,0 +1,16 @@
+/**
+ * @author Anurag
+ * Here we are showing "SOP" can able to print all type of  literals 
+ */
+
+package com.anurag.initial;
+
+public class Basic2 {
+	public static void main(String[] args) {
+		System.out.println(1000);//This is integer
+		System.out.println(10.9090); //This is float
+		System.out.println('s');// This is character
+		System.out.println(true);// This is boolean
+		System.out.println("end");// This is string
+	}
+}
