b4c4b257ee21213f8ea5d1975c22899581d26eed
anurag <test@test.com>
2014-07-27 23:20:41 +0530

    Basic

---
+++ src/com/anurag/initial/Basic18.java
@@ -0,0 +1,17 @@
+package com.anurag.initial;
+/**
+ * 
+ * @author Anurag
+ * 
+ * concept : if method return other than void return statement is mandatory
+ */
+public class Basic18 {
+	static int test(){
+		System.out.println("test");
+		return 10;
+	}
+	 public static void main(String[] args) {
+		int i=test();
+		System.out.println(i);
+	}
+}
