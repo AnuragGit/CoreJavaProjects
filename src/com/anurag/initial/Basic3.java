848a554d27c061b99404a892b0bad8b788824b71
anurag <test@test.com>
2014-07-27 13:15:52 +0530

    In sop we can perfom any numeric operations*

---
+++ src/com/anurag/initial/Basic3.java
@@ -0,0 +1,16 @@
+package com.anurag.initial;
+/**
+ * 
+ * @author Anurag
+ * 
+  In sop we can perfom any numeric operations*
+ *
+ */
+public class Basic3 {
+	public static void main(String[] args) {
+		System.out.println(10);
+		System.out.println(10+20);
+		System.out.println(100*2);
+		System.out.println(true==false);
+	}
+}
