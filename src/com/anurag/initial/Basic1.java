348c236079934b9b0f71fb9e1acae2d49bf24711
anurag <test@test.com>
2014-07-27 12:49:27 +0530

    giving comments messages

--- src/com/anurag/initial/Basic1.java
+++ src/com/anurag/initial/Basic1.java
@@ -1,3 +1,8 @@
+/**
+ * @author Anurag
+ * This is a basic java program
+ * This is a built in structure of java program and we have to follow it.
+ */
 package com.anurag.initial;
 
 public class Basic1 {
