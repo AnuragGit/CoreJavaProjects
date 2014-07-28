ddbf4e87ef9c35780839ea1e24241021d160ecb9
anurag <test@test.com>
2014-07-28 23:26:10 +0530

    Local variable

---
+++ src/com/anurag/global/staticvariable/Local2.java
@@ -0,0 +1,25 @@
+package com.anurag.global.staticvariable;
+/**
+ * @author Anurag
+ * Concept:= This program is showing the scope of local variable.
+ */
+public class Local2 {
+
+	static void test(){
+		int i=10;
+	}
+	
+	public static void main(String[] args) {
+		int i=5;
+		System.out.println(i);
+		test();
+		System.out.println(i);
+	}
+}
+
+/*
+ *  Output:-
+ *        5
+ *        5
+ *         
+ */
